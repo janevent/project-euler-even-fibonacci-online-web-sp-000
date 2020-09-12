f9da820b9dbb9a462168c2cb13fa51a2bfad4e49
Jane Venter <janeanneventer@gmail.com>
2020-09-12 20:01:39 +0000

    Automatically backed up by Learn

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
-# Implement your procedural solution here!
+]']'# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  array = [1, 2]
+  
+  i = 0
+  while i < limit
+    i = array[-2] + array[-1]
+    array << i
+    
+    #fibo = (1..limit).inject([0,1]){ |f, n| f << f[-2] + f[-1] }
+  end
+  
+  newArray = array.select do |item| 
+    item.even?  
+  end
+  
+  newArray.reduce(0) {|total, item| total + item }
+  
+end
