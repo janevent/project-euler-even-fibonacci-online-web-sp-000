a2abcfebdb9b0b01a3924ece5f2dab6000a36ba6
Jane Venter <janeanneventer@gmail.com>
2020-09-12 20:13:51 +0000

    Automatically backed up by Learn

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -5,4 +5,20 @@ class EvenFibonacci
   def initialize(limit)
     @limit = limit
   end
+  
+  def sum 
+    array = [1]
+    i = 2 
+    while i < self.limit 
+      array << i 
+      i = array[-2] + array[-1]
+    end
+    
+    evenArray = array.select do |number|
+      number.even?
+    end
+    
+    evenArray.reduce(0) { | total, number | total + number }
+  end
+  
 end
